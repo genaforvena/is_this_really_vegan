49092e3bafc4886a07444f25b9522dc35465f6d9
Ilya Mozerov <ilyailiilia@gmail.com>
2018-12-06 21:31:05 +0100

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,228 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class CameraHome extends StatefulWidget {
+  @override
+  _CameraHomeState createState() {
+    return _CameraHomeState();
+  }
+}
+
+void logError(String code, String message) =>
+    print('Error: $code\nError Message: $message');
+
+class _CameraHomeState extends State<CameraHome> {
+  CameraController controller;
+  String imagePath;
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  _CameraHomeState() {
+    _selectCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Is this really vegan?'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: Center(
+                  child: imagePath == null
+                      ? _cameraPreviewWidget()
+                      : _takenPicturePreview(),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                border: Border.all(
+                  color: controller != null && controller.value.isRecordingVideo
+                      ? Colors.redAccent
+                      : Colors.grey,
+                  width: 3.0,
+                ),
+              ),
+            ),
+          ),
+          _captureControlRowWidget()
+        ],
+      ),
+    );
+  }
+
+  /// Display the preview from the camera (or a message if the preview is not available).
+  Widget _cameraPreviewWidget() {
+    if (controller == null || !controller.value.isInitialized) {
+      return const Text(
+        'Wait for camera',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24.0,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    } else {
+      return AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: CameraPreview(controller),
+      );
+    }
+  }
+
+  Widget _takenPicturePreview() {
+    return Image.file(File(imagePath));
+  }
+
+  Widget _captureControlRowWidget() {
+    var controls = <Widget>[];
+    if (imagePath == null) {
+      controls.add(IconButton(
+        icon: const Icon(Icons.camera_alt),
+        color: Colors.blue,
+        onPressed: controller != null &&
+            controller.value.isInitialized &&
+            !controller.value.isRecordingVideo
+            ? onTakePictureButtonPressed
+            : null,
+      ));
+    } else {
+      controls.addAll(
+          [
+            IconButton(icon: const Icon(Icons.clear),
+                onPressed: onRetakePicturePressed),
+            IconButton(icon: const Icon(Icons.search),
+                onPressed: null)
+          ]
+      );
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: controls,
+    );
+  }
+
+  void onRetakePicturePressed() {
+    setState(() {
+      imagePath = null;
+    });
+  }
+
+  void onCheckPressed() {
+    showInSnackBar("Check pressed");
+  }
+
+  String timestamp() =>
+      DateTime
+          .now()
+          .millisecondsSinceEpoch
+          .toString();
+
+  void showInSnackBar(String message) {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void _selectCamera() async {
+    final cameraDescription =
+    cameras.firstWhere((c) => c.lensDirection == CameraLensDirection.back);
+
+    if (cameraDescription == null) {
+      showInSnackBar('No back camera detected!');
+      return;
+    }
+
+    if (controller != null) {
+      await controller.dispose();
+    }
+    controller = CameraController(cameraDescription, ResolutionPreset.high);
+
+    // If the controller is updated then update the UI.
+    controller.addListener(() {
+      if (mounted) setState(() {});
+      if (controller.value.hasError) {
+        showInSnackBar('Camera error ${controller.value.errorDescription}');
+      }
+    });
+
+    try {
+      await controller.initialize();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  void onTakePictureButtonPressed() {
+    takePicture().then((String filePath) {
+      if (mounted) {
+        setState(() {
+          imagePath = filePath;
+        });
+      }
+    });
+  }
+
+  Future<String> takePicture() async {
+    if (!controller.value.isInitialized) {
+      showInSnackBar('Error: select a camera first.');
+      return null;
+    }
+    final Directory extDir = await getApplicationDocumentsDirectory();
+    final String dirPath = '${extDir.path}/Pictures/flutter_test';
+    await Directory(dirPath).create(recursive: true);
+    final String filePath = '$dirPath/${timestamp()}.jpg';
+
+    if (controller.value.isTakingPicture) {
+      // A capture is already pending, do nothing.
+      return null;
+    }
+
+    try {
+      await controller.takePicture(filePath);
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
+    return filePath;
+  }
+
+  void _showCameraException(CameraException e) {
+    logError(e.code, e.description);
+    showInSnackBar('Error: ${e.code}\n${e.description}');
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CameraHome(),
+    );
+  }
+}
+
+List<CameraDescription> cameras;
+
+Future<void> main() async {
+  // Fetch the available cameras before initializing the app.
+  try {
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    logError(e.code, e.description);
+  }
+  runApp(MyApp());
+}
